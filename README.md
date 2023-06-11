74b893381cc17b78dfe1293fcc549843e53df0e9
Alvarado08 <jorgealvarado0238@gmail.com>
2023-06-10 22:35:50 -0600

    v1
    Functional v1 of my Calculator

---
+++ README.md
@@ -0,0 +1,20 @@
+# About The Project
+
+![Screenshot of Calculator](preview.png)
+
+[Demo](https://www.theodinproject.com/lessons/foundations-calculator)
+
+[The Odin Project Foundations Course Final Project](https://www.alvarado08.github.io/ca1culator/) consisted in making a Calculator with simple operations and additional extra credit challenges! This was the perfect project to test out my new superpowers! After a few hours of struggle and moments of staring into the screen, I'm really proud to say that I managed to wire things together and bring this to life.
+
+### The Calculator can
+
+- Perform basic operations
+- Handle decimals and round results
+- Chain multiple operations
+- Clear results and delete an unwanted selection
+- Handle uncommon and unwanted operations with error messages
+
+## Built With
+
+- Tailwind
+- JavaScript
--- app.js
+++ app.js
@@ -7,59 +7,130 @@ const clearBtn = document.querySelector("#clear");
 const backspaceBtn = document.querySelector("#backspace");
 
 let result = 0;
-let operand = null;
+let displayString = "0";
+let operand1 = "";
+let operand2 = "";
 let operator = null;
-displayResult.textContent = result;
+displayResult.textContent = displayString;
 
+// Functions
 // Operation Functions
 function add(n1,n2){
-    result = n1 + n2;
-    displayResult.textContent = result;
+    if(!Number.isInteger(result)){
+        result = parseFloat(n1) + parseFloat(n2);
+        displayResult.textContent = result;
+    }else{
+        result = parseInt(n1) + parseInt(n2);
+        displayResult.textContent = result;
+    }
 }
 
-function subtract(){
-    
+function subtract(n1,n2){
+    if(!Number.isInteger(result)){
+        result = parseFloat(n1) - parseFloat(n2);
+        displayResult.textContent = result;
+    }else{
+        result = parseInt(n1) - parseInt(n2);
+        displayResult.textContent = result;
+    }
 }
 
-function multiply(){
-    
+function multiply(n1,n2){
+    if(!Number.isInteger(result)){
+        result = parseFloat(n1) * parseFloat(n2);
+        displayResult.textContent = result;
+    }else{
+        result = parseInt(n1) * parseInt(n2);
+        displayResult.textContent = result;
+    }
 }
 
-function divide(){
-    
+function divide(n1,n2){
+    if(n2 === "0"){
+        zeroError("Tricky 0!", 1000);
+        // displayString = "Tricky 0!"
+        // displayResult.textContent = displayString;
+    }else{
+        if(!Number.isInteger(result)){
+            result = parseFloat(n1) / parseFloat(n2);
+            result = Math.floor(result * 10) / 10;
+            displayResult.textContent = result;
+        }else{
+            result = parseInt(n1) / parseInt(n2);
+            result = Math.floor(result * 10) / 10;
+            displayResult.textContent = result;
+        }
+    }
 }
 
 // Operate Function
 function operate(n1, n2, operation){
-    if(n2 === null){
-        result = "Give me another number to work with!";
-        displayResult.textContent = result;
-    }
-    if(operation === "+"){
-        add(n1,n2);
-    }else if(operation === "-"){
-        subtract();
-    }else if(operation === "x"){
-        multiply();
+    if(n2 === ""){
+        missingOperand("4g0t 1?", 1000);
+        // displayString = "4g0t 1?";
+        // displayResult.textContent = displayString;
     }else{
-        divide();
+        if(operation === "+"){
+            add(n1,n2);
+        }else if(operation === "-"){
+            subtract(n1,n2);
+        }else if(operation === "x"){
+            multiply(n1,n2);
+        }else{
+            divide(n1,n2);
+        }
     }
 }
 
-//Clear & Backspace Functions
+// Clear Function
 function clear(){
-    operand = null;
+    operand1 = "";
+    operand2 = "";
     operator = null;
     result = 0;
-    displayResult.textContent = result;
+    displayString = "0";
+    displayResult.textContent = displayString;
+    operators.forEach(op => {
+        op.classList.remove("bg-[#FDBF58]");
+    })
+}
+
+// Error Timed Messages
+function zeroError(message,duration){
+    displayResult.textContent = message;
+    setTimeout(function() {
+        displayString = operand1;
+        displayResult.textContent = displayString;
+    }, duration);
+}
+
+function missingOperand(message,duration){
+    displayResult.textContent = message;
+    setTimeout(function() {
+        displayString = operand1;
+        displayResult.textContent = displayString;
+    }, duration);
 }
 
 // Event Listeners
 digits.forEach(digit => {
     digit.addEventListener("click", () => {
-        operand = digit.textContent;
-        result = operand;
-        displayResult.textContent = result;  
+        displayString = "";
+        if(operand1 !== "" && operator !== null){
+            operand2 += digit.textContent
+            displayString = operand2;
+            //displayResult.textContent = operand2;
+        }else{
+            operand1 += digit.textContent;
+            displayString = operand1;
+            // displayResult.textContent = displayString;
+            if(operand1.charAt(1) === "."){
+                result = parseFloat(operand1);
+            }else{
+                result = parseInt(operand1);
+            }
+        }
+        displayResult.textContent = displayString;
         // result += parseInt(digit.textContent);
         // displayResult.textContent = result;
     })
@@ -68,13 +139,41 @@ digits.forEach(digit => {
 operators.forEach(op => {
     op.addEventListener("click", () => {
         operator = op.textContent;
-        result += operator;
-        displayResult.textContent = result; 
+        op.classList.add("bg-[#FDBF58]");
+        if(operand2 !== ""){
+            operate(result,operand2,operator);
+            operand2 = "";
+            op.classList.remove("bg-[#FDBF58]")
+        }
+        // result += operator;
+        // displayResult.textContent = result; 
     })
 })
 
 equalBtn.addEventListener("click", () => {
-    operate(result,operand,operator);
+    operate(result,operand2,operator);
+    operand2 = "";
+    operator = null;
+    operators.forEach(op => {
+        op.classList.remove("bg-[#FDBF58]");
+    })
+})
+
+backspaceBtn.addEventListener("click", () => {
+    if(operand1 !== "" && operator === null){
+        operand1 = "";
+        displayString = "0";
+        displayResult.textContent = displayString;
+    }else if(operand1 !== "" && operand2 !== ""){
+        operand2 = "";
+        displayString = "0";
+        displayResult.textContent = displayString;
+    }else if(operand1 !== "" && operator !== null){
+        operator = null;
+        operators.forEach(op => {
+            op.classList.remove("bg-[#FDBF58]");
+        })
+    }
 })
 
 clearBtn.addEventListener("click", clear);
